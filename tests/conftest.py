627758a9cc7c141648a6ce251f9d8d3f4503a323
orbxball <orbxball@protonmail.com>
2020-12-21 21:22:34 -0500

    test on weth mkr delegate strategy

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,52 +1,12 @@
 import pytest
-from brownie import config
-
-@pytest.fixture
-def andre(accounts):
-    # Andre, giver of tokens, and maker of yield
-    yield accounts[0]
+from brownie import Wei, config
 
 
 @pytest.fixture
-def token(andre, Token):
-    yield andre.deploy(Token)
-
-
-@pytest.fixture
-def gov(accounts):
-    # yearn multis... I mean YFI governance. I swear!
-    yield accounts[1]
-
-
-@pytest.fixture
-def rewards(gov):
-    yield gov  # TODO: Add rewards contract
-
-
-@pytest.fixture
-def guardian(accounts):
-    # YFI Whale, probably
-    yield accounts[2]
-
-
-@pytest.fixture
-def vault(pm, gov, rewards, guardian, token):
+def vault(gov, rewards, guardian, currency, pm):
     Vault = pm(config["dependencies"][0]).Vault
-    vault = guardian.deploy(Vault, token, gov, rewards, "", "")
+    vault = guardian.deploy(Vault, currency, gov, rewards, "", "")
     yield vault
-
-
-@pytest.fixture
-def strategist(accounts):
-    # You! Our new Strategist!
-    yield accounts[3]
-
-
-@pytest.fixture
-def keeper(accounts):
-    # This is our trusty bot!
-    yield accounts[4]
-
 
 @pytest.fixture
 def strategy(strategist, keeper, vault, Strategy):
@@ -54,64 +14,89 @@ def strategy(strategist, keeper, vault, Strategy):
     strategy.setKeeper(keeper)
     yield strategy
 
-
 @pytest.fixture
-def nocoiner(accounts):
-    # Has no tokens (DeFi is a ponzi scheme!)
-    yield accounts[5]
-
-
-@pytest.fixture
-def pleb(accounts, andre, token, vault):
-    # Small fish in a big pond
-    a = accounts[6]
-    # Has 0.01% of tokens (heard about this new DeFi thing!)
-    bal = token.totalSupply() // 10000
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
+def Vault(pm):
+    Vault = pm(config["dependencies"][0]).Vault
+    yield Vault
 
 
 @pytest.fixture
-def chad(accounts, andre, token, vault):
-    # Just here to have fun!
-    a = accounts[7]
-    # Has 0.1% of tokens (somehow makes money trying every new thing)
-    bal = token.totalSupply() // 1000
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
-
+def weth(interface):
+    yield interface.ERC20('0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2')
 
 @pytest.fixture
-def greyhat(accounts, andre, token, vault):
-    # Chaotic evil, will eat you alive
-    a = accounts[8]
-    # Has 1% of tokens (earned them the *hard way*)
-    bal = token.totalSupply() // 100
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
-
+def dai(interface):
+    yield interface.ERC20('0x6B175474E89094C44Da98b954EedeAC495271d0F')
 
 @pytest.fixture
-def whale(accounts, andre, token, vault):
-    # Totally in it for the tech
-    a = accounts[9]
-    # Has 10% of tokens (was in the ICO)
-    bal = token.totalSupply() // 10
-    token.transfer(a, bal, {"from": andre})
-    # Unlimited Approvals
-    token.approve(vault, 2 ** 256 - 1, {"from": a})
-    # Deposit half their stack
-    vault.deposit(bal // 2, {"from": a})
-    yield a
+def live_weth_vault(Vault):
+    yield Vault.at('0x6392e8fa0588CB2DCb7aF557FdC9D10FDe48A325')
+
+@pytest.fixture
+def live_dai_vault(Vault):
+    yield Vault.at('0xBFa4D8AA6d8a379aBFe7793399D3DdaCC5bBECBB')
+
+@pytest.fixture
+def live_weth_strategy(Strategy):
+    yield Strategy.at('0x2476eC85e55625Eb658CAFAFe5fdc0FAE2954C85')
+
+@pytest.fixture
+def live_dai_strategy(Strategy):
+    # GenericLevCompFarm
+    yield Strategy.at('0x001F751cdfee02e2F0714831bE2f8384db0F71a2')
+
+
+@pytest.fixture(scope='session')
+def dev(accounts):
+    # Sam's dev account
+    yield accounts.at('0xC3D6880fD95E06C816cB030fAc45b3ffe3651Cb0', force=True)
+
+@pytest.fixture
+def andre(accounts):
+    # Andre, giver of tokens, and maker of yield
+    yield accounts[0]
+
+@pytest.fixture
+def token(andre, Token):
+    yield andre.deploy(Token)
+
+@pytest.fixture
+def gov(accounts):
+    yield accounts.at('0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52', force=True)
+
+@pytest.fixture
+def rewards(gov):
+    yield gov  # TODO: Add rewards contract
+
+@pytest.fixture
+def guardian(accounts):
+    # yield accounts.at('0x846e211e8ba920b353fb717631c015cf04061cc9', force=True)
+    yield accounts[2]
+
+@pytest.fixture
+def vault(gov, rewards, guardian, token, Vault):
+    vault = guardian.deploy(Vault, token, gov, rewards, "", "")
+    yield vault
+
+@pytest.fixture
+def strategist(accounts):
+    # You! Our new Strategist!
+    yield accounts[3]
+
+@pytest.fixture
+def keeper(accounts):
+    # This is our trusty bot!
+    yield accounts[4]
+
+@pytest.fixture
+def whale(accounts, history, web3):
+    # binance7 wallet
+    #acc = accounts.at('0xBE0eB53F46cd790Cd13851d5EFf43D12404d33E8', force=True)
+
+    # binance8 wallet
+    #acc = accounts.at('0xf977814e90da44bfa03b6295a0616a897441acec', force=True)
+
+    # weth whale
+    acc = accounts.at('0xee2826453A4Fd5AfeB7ceffeEF3fFA2320081268', force=True)
+    yield acc
+
