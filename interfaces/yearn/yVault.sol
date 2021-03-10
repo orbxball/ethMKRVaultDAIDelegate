fc3c21e4afaff4f9616ec9dfac4bb1f91735eeb4
orbxball <orbxball@protonmail.com>
2021-03-10 16:14:57 -0500

    change compiler version on interfaces

--- interfaces/yearn/yVault.sol
+++ interfaces/yearn/yVault.sol
@@ -1,15 +1,15 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.6.12;
+pragma solidity >=0.6.12;
 
 interface yVault {
+    function deposit() external;
+
     function deposit(uint256) external;
 
-    function depositAll() external;
+    function withdraw() external;
 
     function withdraw(uint256) external;
-
-    function withdrawAll() external;
 
     function pricePerShare() external view returns (uint256);
 }
