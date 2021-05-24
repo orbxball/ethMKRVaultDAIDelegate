f566f02205d1ee833d8cace34bccfa8e2e49e23e
orbxball <orbxball@protonmail.com>
2021-05-24 12:21:57 -0400

    feat: test emergency exit thoroughly

--- tests/test_emergency.py
+++ tests/test_emergency.py
@@ -1,9 +1,19 @@
 import pytest
+from brownie import chain
 
 
-def test_emergency_exit(gov, vault, strategy, token):
-    strategy.setEmergencyExit({"from": gov})
+def test_emergency_exit(gov, vault, strategy, token, whale, amount, dai):
+    scale = 10 ** token.decimals()
+    # Deposit to the vault
+    token.approve(vault, 2 ** 256 - 1, {"from": whale})
+    vault.deposit(amount, {"from": whale})
+    assert token.balanceOf(vault.address) == amount
     strategy.harvest({"from": gov})
 
-    assert vault.totalDebt() == 0
+    dai.transfer(strategy, 1, {"from": gov})
+
+    strategy.setEmergencyExit({"from": gov})
+    strategy.harvest({"from": gov})
+    print(f"strategy info: {vault.strategies(strategy).dict()}")
+
     assert vault.totalAssets() == token.balanceOf(vault)
